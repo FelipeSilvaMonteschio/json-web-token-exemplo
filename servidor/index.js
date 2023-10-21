e7a84fd46b2d3894f5fce5ab3370e519d4363c19
FelipeSilvaMonteschio <felipe.monteschio@estudante.ifms.edu.br>
2023-10-21 10:47:40 -0400

    comit

--- servidor/index.js
+++ servidor/index.js
@@ -8,17 +8,14 @@ var cookieParser = require('cookie-parser')
 
 const express = require('express');
 const { usuario } = require('./models');
+const crypto = require('./crypto')
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(cors());
-
-const crypto = require('./crypto')
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-
 app.use(cookieParser());
 app.use(
   expressJWT({
@@ -28,50 +25,35 @@ app.use(
   }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/usuarios/cadastrar"] })
 );
 
-
-
-
+// Pagina Autenticar
 app.get('/autenticar', async function(req, res){
   res.render('autenticar');
 })
 
+// Autenticar
+app.post('/logar',(req, res)=> {
+  let Nome = req.body.usuario; let Senha = req.body.senha
+
+  if(Nome === "felipe@teste" && Senha === "123"    ){
+    const id = '1'
+    const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 30000 })
+    res.cookie('token', token)
+    return res.redirect('/')
+  }
+  res.status(500).json({ mensagem: "login Inválido" })
+})
+
+//Home
 app.get('/', async function(req, res){
   res.render("home")
 })
 
-app.get('/usuarios/listar',async function(req, res){
-
-  let usuarios = await usuario.findAll()
-
-  res.render('listauser', {usuarios});
-})
-
+// Pagina de Cadastro
 app.get('/usuarios/cadastrar', async function(req, res){
   res.render('cadastro');
 })
 
-app.post('/logar', (req, res) => {
-  const dados = req.body;
-  let usuarioT = usuario.findOne({ where: { usuario: req.query.nome } })
-  let usuarioS = usuario.findOne({ where: { senha: req.query.senha } })
-  
-  if(dados.usuario === "felipe@teste" && dados.senha === "123" || dados.usuario == usuarioT  ){
-    const id = '1'
-    const token = jwt.sign({ id }, process.env.SECRET, {
-      expiresIn: 30000
-    })
-    res.cookie('token', token, { httpOnly: true })
-    return res.redirect('/') //home
-  }
-  res.status(500).json({ mensagem: "login Inválido" })
-  console.log(usuarios)
-})
-
-app.post('/deslogar', function(req, res) {
-  res.cookie('token', null, { httpOnly: true })
-  res.redirect('/autenticar')
-})
-
+// Cadastro
 app.post('/usuarios/cadastrar', async function(req, res){
 
   if(req.body.senha === req.body.senhacf){
@@ -82,9 +64,23 @@ app.post('/usuarios/cadastrar', async function(req, res){
   }else{
     console.log('usuario não cadastrado tente novamente')
   }
-  
 })
 
+// Lista de usuarios
+app.get('/usuarios/listar',async function(req, res){
+
+  let usuarios = await usuario.findAll()
+
+  res.render('listauser', {usuarios});
+})
+
+// Deslogar
+app.post('/deslogar', function(req, res) {
+  res.cookie('token', null)
+  res.redirect('/autenticar')
+})
+
+// Servidor Test
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!')
 });
