24011f8c3d5aca0a2570a22dd5fa12d5e5b48b11
FelipeSilvaMonteschio <felipe.monteschio@outlook.com>
2023-12-05 10:41:33 -0400

    final

--- cliente/src/app/componentes/ListaUsers.js
+++ cliente/src/app/componentes/ListaUsers.js
@@ -1,10 +1,12 @@
+import styles from '@/app/css/style.module.css'
+
 export default async function ListaUsers({users}) {
    await new Promise((resolve) => setTimeout(resolve, 1000));
    return (
-     <div>
+     <div >
        <center>
        {users?.map((user,index) =>
-        <p key={index}>
+        <p key={index}className={styles.card}>
            {user.nome}
            <br/>
            {user.senha}
